e9ca8109589cb8ebd1f58d1336877cf1b751148a
tushar-hossain <tusharhossain455@gmail.com>
2025-08-21 01:16:39 +0600

    api todo data fetch and ui data showing done

--- src/Pages/AllTodo/AllTodo.jsx
+++ src/Pages/AllTodo/AllTodo.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
+import app from "../../firebase/firebase.init";
+import { getDatabase, ref, get } from "firebase/database";
+import TodoCard from "./TodoCard";
 
 function AllTodo() {
+  const fetchData = async () => {
+    const db = getDatabase(app);
+    const dbref = ref(db, "todoDB/todos");
+    const todos = await get(dbref);
+    if (todos.exists()) {
+      return Object.values(todos.val());
+    } else {
+      alert("error");
+    }
+  };
+
   return (
     <div>
-      <h1>Todo</h1>
+      <h1 style={{ textAlign: "center" }}>Todo</h1>
+      <div>
+        <Suspense fallback={<p>Loading....</p>}>
+          <TodoCard todoData={fetchData()} />
+        </Suspense>
+      </div>
     </div>
   );
 }
